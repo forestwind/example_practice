09a554a206d14415d84593df56a4b0b0e0ed47e2
Lee Jin-Woo <came1230@naver.com>
2016-05-04 03:25:33 +0900

    애니메이션 블렌딩 전까지

---
+++ Unity3D/unity3d_wikibook/Assets/02.Scripts/FollowCam.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+
+    public Transform targetTr;
+    public float dist = 10.0f;
+    public float height = 3.0f;
+    public float dampTrace = 20.0f;
+
+    private Transform tr;
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+	}
+	
+	
+	void LateUpdate () {
+        tr.position = Vector3.Lerp(tr.position, 
+            targetTr.position - (targetTr.forward * dist) + (Vector3.up * height), 
+            Time.deltaTime * dampTrace);
+
+        tr.LookAt(targetTr.position);
+	}
+}
---
+++ Unity3D/unity3d_wikibook/Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtrl : MonoBehaviour {
+
+
+    [System.Serializable]
+    public class Anim
+    {
+        public AnimationClip idle;
+        public AnimationClip runForward;
+        public AnimationClip runBackward;
+        public AnimationClip runRight;
+        public AnimationClip runLeft;
+    }
+
+	private float h = 0.0f;
+	private float v = 0.0f;
+
+    private Transform tr;
+
+    public float moveSpeed = 10.0f;
+    public float rotSpeed = 100.0f;
+
+    public Anim anim;
+
+    public Animation _animation;
+
+    // Use this for initialization
+    void Start () {
+
+        tr = GetComponent<Transform>();
+
+        _animation = GetComponentInChildren<Animation>();
+
+        _animation.clip = anim.idle;
+        _animation.Play();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Debug.Log("H = " + h.ToString());
+        Debug.Log("V = " + v.ToString());
+
+        Vector3 moveDir = ( Vector3.forward * v ) + ( Vector3.right * h );
+
+        tr.Translate( moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+
+        tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+    }
+}
